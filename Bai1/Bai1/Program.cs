a705ecf0c2ae4cd6fb99d036dcc52245778801f6
Van Phuong <email_cua_ban@gmail.com>
2026-03-16 01:59:19 +0700

    Bài 1: Tạo danh sách số nguyên

---
+++ Bai1/Bai1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong");
+            List<int> so = new List<int>();
+            Console.WriteLine("Nhap 5 so bat ky");
+            for(int i =0; i < 5; i++)
+            {
+                Console.WriteLine($"Nhap so thu {i}: ");
+                string Nhap = Console.ReadLine();
+                int n = int.Parse(Nhap);
+                so.Add(n);
+            }
+            for (int i = 0; i <5; i++)
+            {
+                Console.Write(so[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
