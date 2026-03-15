3d77d118608d16a93d428e311e0e4e93d164e314
Van Phuong <email_cua_ban@gmail.com>
2026-03-16 02:25:10 +0700

    Bài2: Tính tổng các phần tử

---
+++ Bai2/Bai2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV:2415053122230");
+            Console.WriteLine("Vo Van Phuong");
+
+            Console.WriteLine("nhap do dai day so: ");
+            int n = int .Parse(Console.ReadLine());
+
+            List<int> danhsach = new List<int>();
+            Console.WriteLine("Nhap cac so nguyen");
+            for(int i= 0; i < n; i++)
+            {
+                Console.WriteLine($"Nhap so {i+1}:  " );
+                danhsach.Add(int.Parse(Console.ReadLine()));
+            }
+            int tong = 0;
+            for(int i= 0;i < n; i++)
+            {
+                tong = tong + danhsach[i];
+
+            }
+            Console.WriteLine("ket qua cua day so la: "+ tong);
+        }
+    }
+}
