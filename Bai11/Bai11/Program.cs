58f1551e0ee7f170979013b6b0ae17cbfd0a31ca
Van Phuong <email_cua_ban@gmail.com>
2026-03-16 23:52:34 +0700

    Bài 11. Lưu danh sách sinh viên

---
+++ Bai11/Bai11/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Bai11
+{
+    internal class Program
+    {
+        public class Student
+        {
+            public int Id;
+            public string Name;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong");
+
+            List<Student> danhsachsinhvien = new List<Student>();
+            Student sv1= new Student();
+            sv1.Id = 1;
+            sv1.Name = "Vo Van Phuong";
+
+            Student sv2 = new Student();
+            sv2.Id = 2;
+            sv2.Name = "Vo Thi quynh duyen";
+            danhsachsinhvien.Add(sv1);
+            danhsachsinhvien.Add(sv2);
+            foreach(Student sv in danhsachsinhvien)
+            {
+                Console.WriteLine("Ma sv:" + sv.Id+ " Ho ten: " + sv.Name);
+
+            }
+        }
+    }
+}
