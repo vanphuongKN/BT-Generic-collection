75f4e9567d754fc8c59d38e06ccd8717fa6ca31b
Van Phuong <email_cua_ban@gmail.com>
2026-03-16 23:25:09 +0700

    Bài 10. Tìm phần tử nhỏ nhất

---
+++ Bai10/Bai10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Bai10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong");
+
+            List<int> danhSach = new List<int> { 45, 12, 56, 8, 23, 99 };
+            int soNhoNhat = danhSach[0];
+            for (int i = 1; i < danhSach.Count; i++)
+            {
+                int soHienTai = danhSach[i];
+                if(soHienTai <  soNhoNhat)
+                    soNhoNhat = soHienTai;
+
+            }
+            Console.WriteLine("gia tri nho nhat trong danh sach: " + soNhoNhat);
+
+        }
+    }
+}
