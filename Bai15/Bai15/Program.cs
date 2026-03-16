6a808128ad21ddb1b6ee9e28318c1cf4851c0eb4
Van Phuong <email_cua_ban@gmail.com>
2026-03-17 01:43:11 +0700

    Bài 15. Sử dụng Dictionary

---
+++ Bai15/Bai15/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Bai15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> tudien = new Dictionary<string, string>();
+
+            tudien.Add("hello", "xin chao");
+            tudien.Add("book", "quyen sach");
+            tudien.Add("computer", "may tinh");
+            tudien.Add("school", "truong hoc");
+
+            Console.Write("Nhap tu tieng Anh can tra: ");
+            string tu = Console.ReadLine().ToLower();
+
+            if (tudien.ContainsKey(tu))
+            {
+                Console.WriteLine("Nghia tieng Viet: " + tudien[tu]);
+            }
+            else
+            {
+                Console.WriteLine("Khong tim thay tu nay trong tu dien");
+            }
+        }
+    }
+}
