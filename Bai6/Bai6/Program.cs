a18c5d0754fec7cfbf4c489288cf0b6065e8acb6
Van Phuong <email_cua_ban@gmail.com>
2026-03-16 12:28:15 +0700

    Bài6: Kiểm tra phần tử tồn tại

---
+++ Bai6/Bai6/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Bai6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong");
+
+            List<string> traiCay = new List<string> { "Dao", "Chuoi", "Cam", "Xoai", "Man" };
+            Console.Write("Nhap ten loai trai cay ban muon tim: ");
+            string timKiem = Console.ReadLine();
+            if (traiCay.Contains(timKiem))
+            {
+                Console.WriteLine($"trai '{timKiem}' co trong danh sach.");
+            }
+            else
+            {
+                Console.WriteLine($"trai '{timKiem}' khong co trong danh sach.");
+            }
+        }
+    }
+}
