8f523fb640cc399abf4e3659b19cde7462cd36bf
Van Phuong <email_cua_ban@gmail.com>
2026-03-16 22:06:15 +0700

    Bài 8. Đảo ngược danh sách

---
+++ Bai8/Bai8/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Bai8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong");
+
+
+            List<int> danhsach = new List<int>() { 1,24, 51,2,64 };
+            danhsach.Reverse();
+            foreach (int i in danhsach)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
